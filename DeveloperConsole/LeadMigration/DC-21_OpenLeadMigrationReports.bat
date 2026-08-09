7d9e507f3cdacbaaeaa2a6c40635c8ff5b89547d
Ulysses A. Barnes, Jr. <melrosegroupbroker@gmail.com>
2026-08-09 05:09:25 -0500

    MOS5-016 Enterprise Build 2026.08.09-050921

--- DeveloperConsole/LeadMigration/DC-21_OpenLeadMigrationReports.bat
+++ DeveloperConsole/LeadMigration/DC-21_OpenLeadMigrationReports.bat
@@ -1,1 +1,6 @@
 @echo off
+setlocal EnableExtensions
+set "REPORTS=D:\MelroseOS\GitHub\MelroseOS\tools\LeadMigration\Reports"
+if not exist "%REPORTS%" mkdir "%REPORTS%"
+start "" explorer "%REPORTS%"
+exit /b 0
--- DeveloperConsole/LeadMigration/DC-22_OpenLeadMigrationActive.bat
+++ DeveloperConsole/LeadMigration/DC-22_OpenLeadMigrationActive.bat
@@ -1,1 +1,11 @@
 @echo off
+setlocal EnableExtensions
+set "ACTIVE=D:\MelroseOS\GitHub\MelroseOS\tools\LeadMigration\Active"
+if not exist "%ACTIVE%" (
+    echo [FAIL] Active module folder not found:
+    echo %ACTIVE%
+    pause
+    exit /b 1
+)
+start "" explorer "%ACTIVE%"
+exit /b 0
